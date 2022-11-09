e4d60cb228ff26a922ea37d845ebc6fd925fcf4c
jbsouthe <jbsouthe@github.com>
2022-11-09 12:16:44 -0600

    three tests in one

--- src/main/java/com/cisco/josouthe/RoutingMsgEntryDecorator.java
+++ src/main/java/com/cisco/josouthe/RoutingMsgEntryDecorator.java
@@ -12,7 +12,9 @@ import java.util.List;
 
 public class RoutingMsgEntryDecorator extends AEntry {
 
-    IReflector readIndex, readString, clone, readFieldOrderIfDiff, readNull, readInt, skipBytes, pos;
+    IReflector readIndex, readString, readFieldOrderIfDiff, readNull, readInt, skipBytes, pos;
+
+    IReflector getDecoderWithoutResolver, getBackupDecoder, configure;
 
     public RoutingMsgEntryDecorator() {
         super();
@@ -20,20 +22,41 @@ public class RoutingMsgEntryDecorator extends AEntry {
 
         readIndex = getNewReflectionBuilder().invokeInstanceMethod("readIndex", true).build(); //Integer if!=1 then no data left to read
         readString = getNewReflectionBuilder().invokeInstanceMethod("readString", true).build(); //String
-        clone = getNewReflectionBuilder().invokeInstanceMethod("clone", true).build(); // Object if this does not work use the Builder(RoutingMsg other) method
         readFieldOrderIfDiff = getNewReflectionBuilder().invokeInstanceMethod("readFieldOrderIfDiff", true).build(); //Schema.Field[]
         readNull = getNewReflectionBuilder().invokeInstanceMethod("readNull", true).build(); //void
         readInt = getNewReflectionBuilder().invokeInstanceMethod("readInt", true).build(); //int
         skipBytes = getNewReflectionBuilder().invokeInstanceMethod("skipBytes", true).build(); //Bytes
         pos = getNewReflectionBuilder().invokeInstanceMethod("pos", true).build(); // int
+
+        getDecoderWithoutResolver = getNewReflectionBuilder().accessFieldValue("in", true).build(); //returns a org.apache.avro.io.Decoder
+
+        getBackupDecoder = getNewReflectionBuilder().accessFieldValue("backup", true).build(); //returns a org.apache.avro.io.Decoder
+        configure = getNewReflectionBuilder().invokeInstanceMethod("configure", true, new String[]{ "org.apache.avro.io.Decoder" }).build(); //resets the input
     }
 
     //This language "unmarshalTransactionContext returns a string value of the singularityHeader
     @Override
     public String unmarshalTransactionContext(Object invokedObject, String className, String methodName, Object[] paramValues, ISDKUserContext context) throws ReflectorException {
-        Object resolvingDecoder = clone.execute(invokedObject.getClass().getClassLoader(), paramValues[0]); // https://avro.apache.org/docs/1.9.2/api/java/org/apache/avro/io/ResolvingDecoder.html
+        Object resolvingDecoder = paramValues[0]; // https://avro.apache.org/docs/1.9.2/api/java/org/apache/avro/io/ResolvingDecoder.html
+        long startTimestamp = System.currentTimeMillis();
+        //the avro resolving decoder makes a backup of the decoder and copies the backup over the decoder after it reaches the end, this is self rewinding, no clone needed!
+        /*ie:
+            if (top instanceof Symbol.DefaultStartAction) {
+                Symbol.DefaultStartAction dsa = (Symbol.DefaultStartAction)top;
+                this.backup = this.in;
+                this.in = DecoderFactory.get().binaryDecoder(dsa.contents, null);
+            } else if (top == Symbol.DEFAULT_END_ACTION) {
+                this.in = this.backup;
+            }
+         */
 
-        //cloned the Resolving Decoder before the method runs, hoping that means we will have content we can read independent of the parameter in the cloned object
+        /* technique number 1 is to just use the resolving decoder and allow the method above to reset it after it is read,
+
+        if that fails let's uncomment this next line and just use the decoder in the raw, which seems to skip schema validation:
+        resolvingDecoder = getDecoderWithoutResolver.execute(invokedObject.getClass().getClassLoader(), resolvingDecoder);
+        */
+
+
         try {
             Object[] fieldOrder = (Object[]) readFieldOrderIfDiff.execute(invokedObject.getClass().getClassLoader(), readFieldOrderIfDiff);
             Integer messageId = (Integer) readInt.execute(invokedObject.getClass().getClassLoader(), resolvingDecoder);
@@ -74,7 +97,7 @@ public class RoutingMsgEntryDecorator extends AEntry {
             context.stashObject("messageId", messageId);
             context.stashObject("schema", schema);
         } catch ( ReflectorException reflectorException ) {
-            getLogger().info(String.format("Oops, while trying to use a cloned ResolvingDecoder, we had an exception, '%s'",reflectorException), reflectorException);
+            getLogger().info(String.format("Oops, while trying to use a ResolvingDecoder, we had an exception, '%s'",reflectorException), reflectorException);
             throw reflectorException;
         }
 
@@ -83,7 +106,22 @@ public class RoutingMsgEntryDecorator extends AEntry {
         if( indexValue == 1 ) {
             transactionContext = (String) readString.execute(invokedObject.getClass().getClassLoader(), resolvingDecoder);
         }
-        getLogger().info(String.format("We found a RoutingMsg tried to get correlation string, got '%s' from index value %d", transactionContext, indexValue));
+
+        /*if that also fails for any number of reasons, we can assume that the ResolvingDecoder is not resetting, confirms data read matches the schema and the read is destructive,
+        this means we need to fall through all this and at the end of it we will need to use the builder to make a new decoder for the program to read.
+        in reading the avro source, this is HIGHLY unlikely so we can assume all that is not needed.
+
+        If the state of the decoder is "bad" let's uncomment this next few lines and allow the decoder to reset the input with the backup manually before we enter into this method
+            MAKE SURE TO COMMENT OUT LINE 56, if you go this route as the internal decoder does not have a configure method
+        try {
+            Object backupDecoder = getBackupDecoder.execute(resolvingDecoder.getClass().getClassLoader(), resolvingDecoder);
+            configure.execute(resolvingDecoder.getClass().getClassLoader(), resolvingDecoder, new Object[]{ backupDecoder });
+        } catch ( ReflectorException reflectorException ) {
+            getLogger().info(String.format("Oops, while trying to reset the internal decoder, we had an exception, '%s'",reflectorException), reflectorException);
+            throw reflectorException;
+        }
+         */
+        getLogger().info(String.format("We found a RoutingMsg tried to get correlation string, got '%s' from index value %d, this took %d ms", transactionContext, indexValue, System.currentTimeMillis()-startTimestamp));
         return transactionContext;
     }
 
